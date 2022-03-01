2a5f6173d246d874247b16de9e04745aa3333470
L.S <L.S.1030@outlook.com>
2022-03-01 12:10:01 +0800

    Project Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Image Resize
+
+## 简介
+
+图像缩放
+
+## 环境配置
+
+Windows10+clion+Ubuntu18.04(with OpenCV 4.5)
---
+++ Src/main.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file main.cpp
+ * @date 2022/3/1
+ * @author L.S.
+*/
+
+#include <cstdio>
+
+int main()
+{
+  printf("Project Init!\n");
+
+  return 0;
+}
